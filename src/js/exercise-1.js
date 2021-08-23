686781b17d46821a6df174ee51de00beee3495d7
Sergiy Bubenko <bubenko.s91@gmail.com>
2021-08-24 01:40:38 +0300

    ok

--- src/js/exercise-1.js
+++ src/js/exercise-1.js
@@ -1,15 +1,13 @@
 const delay = ms => {
-  return new Promise(() => {
+  return new Promise((resolve) => {
   setTimeout(() => {
-    logger(ms)
+    resolve(ms)
   }, ms)  
 }) 
 };
 
-
 const logger = time => console.log(`Resolved after ${time}ms`);
 
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(2000).then(logger);
+delay(1000).then(logger);
+delay(1500).then(logger);
